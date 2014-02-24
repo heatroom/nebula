983aef13b8224254e799383d340e11d99b7d2215
foriacus <634844533@qq.com>
2014-02-24 19:41:30 +0800

    update the img and swf

--- server.js
+++ server.js
@@ -15,11 +15,24 @@ app.configure(function(){
 });
  
 app.get('/foo', function(req, res){ 
-  // important - you have to use the response.json method
+  // important - you have to use the response.jsonp method
   res.jsonp({ 
-  	imgUrl: 'http://iwan.sogou.com',
-  	imgSrc: 'http://imgstore02.cdn.sogou.com/app/a/53/0a4db766-0a44-428d-a29a-6f6822b2a912.jpg',
-  	promote: 'http://wangmeng.sogou.com/',
+    'type': 1, 
+    'data': {
+      'imgUrl': 'http://iwan.sogou.com',
+      'imgSrc': 'http://imgstore02.cdn.sogou.com/app/a/53/0a4db766-0a44-428d-a29a-6f6822b2a912.jpg',
+      'promote': 'http://wangmeng.sogou.com/'
+    }
+  });
+});
+
+app.get('/bar', function(req, res){ 
+  res.jsonp({ 
+    'type': 2, 
+    'data': {
+      'swfSrc': 'http://img01.sogoucdn.com/app/a/4001/4b38275f-8670-44ab-9c19-1ea48821b1b7.swf',
+      'swfUrl': 'http://iwan.sogou.com'
+    }
   });
 });
  
--- src/ad.js
+++ src/ad.js
@@ -14,6 +14,22 @@
 		return qs;
 	}
 
+  function createSwf(swfDivId, swfUrl){
+    var swfVersionStr = "9.0.0";
+    var xiSwfUrlStr = "";
+    var attributes = {};
+    var flashvars = {};
+    var params = {};
+    params.quality = "high";
+    params.allowscriptaccess = "always";
+    params.allowfullscreen = "true";
+    params.wmode = "opaque";
+    attributes.id = "right-pz-swf-obj";
+    var width = 175;
+    var height = 300;
+    swfobject.embedSWF(swfUrl, swfDivId, width,height,swfVersionStr,xiSwfUrlStr,flashvars,params,attributes);
+  }
+
 	var jsonpfunc = 'nebula' + new Date().valueOf();
 
   function jsonp(url, callback) {
@@ -27,28 +43,45 @@
 
   function adCard(){
   	var url = baseurl;
-  	var template = '<div id="div_ad" class="sogou" style="text-align:left;vertical-align:middle">'
+  	var img_tpl = '<div id="div_ad" class="sogou" style="text-align:left;vertical-align:middle">'
         + '<img style="position:absolute;z-index:0" src="#imgSrc" border="0" width="250px;" height="250px;" margin-top="0px;">'
         + '<a href="#imgUrl" target="_blank" style="vertical-align:middle;cursor:pointer;z-index:1;position:absolute;left:0px;top:0px;display:block;background:white;filter:alpha(opacity=0);opacity:0;width:250px;height:250px;"></a>'
         + '<a id="logo" class="logo" style="z-index:2" href="#promote" target="_blank"></a></div>';
+
+    var swf_tpl = '<span id="swf-container"></span>'
+        + '<a target="_blank" class="pz-rt-stat" href="#swfUrl"></a>';
+    var card = doc.createElement('div');
+
   	jsonp(url, function(response) {
-      console.log(response);
-      var card = doc.createElement('div');
-
-      var imgUrl = response['imgUrl'];
-      var imgSrc = response['imgSrc'];
-      var promote = response['promote'];
-
       
-      template = template.replace('#imgUrl', imgUrl);
-      template = template.replace('#imgSrc', imgSrc);
-  		template = template.replace('#promote', promote);
-  		card.innerHTML = template;
-  		doc.body.appendChild(card);
+      var data = response.data;
+      
+      if (response.type === 1) {
+        var imgUrl = data['imgUrl'];
+        var imgSrc = data['imgSrc'];
+        var promote = data['promote'];
+        
+        img_tpl = img_tpl.replace('#imgUrl', imgUrl);
+        img_tpl = img_tpl.replace('#imgSrc', imgSrc);
+        img_tpl = img_tpl.replace('#promote', promote);
+        card.innerHTML = img_tpl;
+        doc.body.appendChild(card);
+      } else if (response.type === 2) {
+        var swfSrc = data['swfSrc'];
+        var swfUrl = data['swfUrl'];
+        swf_tpl = swf_tpl.replace('#swfUrl', swfUrl);
+        console.log(swf_tpl);
+        card.innerHTML = swf_tpl;
+        doc.body.appendChild(card);
+        createSwf("swf-container", swfSrc);
+      }
   	});
+
+
+
+    
   }
 
   adCard();
-
 
 })(document);
